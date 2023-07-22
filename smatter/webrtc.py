539063014cd076c2e9eeeec24ef956d7729540eb
Edward McLoughlin <edward.mcloughlin@gmail.com>
2023-07-22 21:23:45 +1000

    First attempt at data connection for subtitles

--- smatter/webrtc.py
+++ smatter/webrtc.py
@@ -9,17 +9,20 @@ import multiprocessing as mp
 from .utils import QueueIO
 from multiprocessing.synchronize import Event
 from aiohttp import web
-from aiortc import RTCPeerConnection, RTCSessionDescription
+from aiortc import RTCPeerConnection, RTCSessionDescription, RTCDataChannel
 from aiortc.contrib.media import MediaPlayer
 
 class SmatterRTCServer():
-  def __init__(self, stop: Event, _logger: loguru.Logger, passthrough_queue: mp.Queue, file_root=Path('./')):
+  def __init__(self, stop: Event, _logger: loguru.Logger, passthrough_queue: mp.Queue, subtitle_queue: mp.Queue, file_root=Path('./')):
+    self.stopper = stop
     self.file_root = file_root
     self.peer_connections = set()
     self._logger = _logger
     self.passthrough_queue = passthrough_queue
+    self.subtitle_queue = subtitle_queue
     self.pipe = QueueIO('r', passthrough_queue)
     self.media_player = MediaPlayer(file=self.pipe)
+    self.end_signal = mp.Event()
 
   def prep_shutdown(self):
     async def on_shutdown(app):
@@ -63,6 +66,18 @@ class SmatterRTCServer():
 
       pc.addTrack(self.media_player.audio)
       pc.addTrack(self.media_player.video)
+      data_channel = pc.createDataChannel('smatter_vtt')
+      def send_subs():
+        if self.end_signal.is_set():
+          return
+        next = 'DUMMY'
+        while not self.stopper.is_set() and data_channel.bufferedAmount <= data_channel.bufferedAmountLowThreshold and (next:= self.subtitle_queue.get()):
+          data_channel.send(next)
+        if not next:
+          self.end_signal.set()
+
+      data_channel.on('bufferedamountlow', send_subs)
+      data_channel.on('open', send_subs)
 
       # handle offer
       await pc.setRemoteDescription(offer)
@@ -84,12 +99,13 @@ def web_rtc_server(
     stop: Event,
     _logger: loguru.Logger,
     passthrough_queue: mp.Queue,
+    subtitle_queue: mp.Queue,
     front_end_dir: Path,
     host: str,
     port: int
   ):
   app = web.Application()
-  rtcs = SmatterRTCServer(stop, _logger, passthrough_queue, front_end_dir)
+  rtcs = SmatterRTCServer(stop, _logger, passthrough_queue, subtitle_queue, front_end_dir)
   app.on_shutdown.append(rtcs.prep_shutdown())
   app.router.add_get("/", rtcs.prep_index())
   app.router.add_get("/client.js", rtcs.prep_javascript())
